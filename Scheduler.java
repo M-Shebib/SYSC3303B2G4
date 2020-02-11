16ffd10f02dc0e643452f8fd1442f99eee644254
jordanpeterkin <60662777+jordanpeterkin@users.noreply.github.com>
2020-02-11 11:42:42 -0500

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -1,20 +1,28 @@
 package project;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * Subsystem that communicates with the floors and elevator through the control
  *
  */
-public class Scheduler implements Runnable{
+public class Scheduler {
 	public int[] floors;
 	public double inputTime;
-	public Control control;
+	public List<Integer> inputFloors, destinations, elevatorList; //List of aspects of the elevator
+	public boolean dataIn, dataOut;
 	
 	/**
 	 * Simple constructor which connects the scheduler with the controller
 	 * @param control is a class which allows for the three threads to communicate
 	 */
-	public Scheduler(Control control) {
-		this.control = control;
+	public Scheduler() {
+		destinations = new ArrayList<Integer>();
+		elevatorList = new ArrayList<Integer>();
+		inputFloors = new ArrayList<Integer>();
+		dataIn = false;
+		dataOut = false;
 	}
 	/**
 	 * Confirms that data has been received from floor and sets data out to true.
@@ -23,28 +31,20 @@ public class Scheduler implements Runnable{
 	 * @param inputFloor the floor that the elevator is on when it receives the command
 	 * @param destination the floor that the elevator will be going towards.
 	 */
-	public void recieveData(int inputFloor, int destination) {
+	public synchronized void receiveData(int inputFloor, int destination) {
 		System.out.print("Data recieved from floor " + inputFloor + " to bring passenger to floor " + destination + "\n");
-		control.dataOut = true;
+		dataOut = true;
+		elevatorList.add(inputFloor);
+		elevatorList.add(destination);
+		inputFloors.remove(0);
+		destinations.remove(0);
 	}
 	
-	public void run() {
-		while (true) {
-			//This if statement confirms that there is an entity in the elevator and that there has been data put in the floor
-			if(!control.destination.isEmpty() && control.dataIn) {
-				//prepares for the elevator thread to run and makes sure that the control is updated for the situation
-				recieveData(control.inputFloor.get(0), control.destination.get(0));
-				control.elevatorList.add(control.inputFloor.get(0));
-				control.elevatorList.add(control.destination.get(0));
-				control.inputFloor.remove(0);
-				control.destination.remove(0);
-			}
-			//prevents unnecessary code execution when if statement isn't met.
-			try {
-				Thread.sleep(100);
-			}catch (InterruptedException e) {
-				
-			}
-		}
+	public static void main(String[] args) {
+		Scheduler scheduler = new Scheduler();
+		Thread elevator = new Thread(new Elevator(1, scheduler), "elevator1");
+		Thread floor = new Thread(new Floor(scheduler), "floorsystem");
+		floor.start();
+		elevator.start();
 	}
 }
