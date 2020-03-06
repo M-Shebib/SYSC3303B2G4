8a13d2f3bca4644f820e7c8b3d8d6959514e2499
TamerIbrahim97 <tameribrahim89@gmail.com>
2020-03-06 14:38:11 -0500

    Update Floor.java

--- Floor.java
+++ Floor.java
@@ -1,73 +1,124 @@
-package project;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.*;
+import java.util.Arrays;
+import java.util.List;
 /**
  * This thread is for the floor subsystem which knows what the initial floor is
  * as well as reads to determine the target floor.
  */
 public class Floor implements Runnable{
 	
-	public Control control;
 	public BufferedReader input;
 	public String instructions;
 	public int currentTime, inputTime;
 	public int floorNumber, destination;
+	public List<Integer> inputFloors, destinations;
+	public Scheduler scheduler;
+	DatagramPacket sendPacket, receivePacket;
+	DatagramSocket sendReceiveSocket;
+	private InetAddress schedulerIP;
+	private int schedulerPort = 420;
 	/**
 	 * Creates an instance of the floor thread connected to the control.
 	 * As well as attempts to read a file that has the inputs.
-	 * @param control This is the controller that connects the three threads
 	 */
-	public Floor(Control control) {
+	public Floor(Scheduler scheduler, String inputFile) {
+		this.scheduler = scheduler;
 		try {
-			input = new BufferedReader(new FileReader("ElevatorData.txt"));
+			input = new BufferedReader(new FileReader(inputFile));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		this.control = control;
+	}
+	public Floor() {
+		try {
+			schedulerIP = InetAddress.getLocalHost();
+		}catch(UnknownHostException e) {
+			e.printStackTrace();
+		}
+	      try {
+		         sendReceiveSocket = new DatagramSocket();
+		         
+		      } catch (SocketException se) {
+		         se.printStackTrace();
+		         System.exit(1);
+		      }
 	}
 	/**
-	 * Takes the data held in the input buffered reader and seperates it into values that
+	 * Takes the data held in the input buffered reader and separates it into values that
 	 * are able to be used by the scheduler and Elevator.
 	 */
 	public void readData() {
 		String[] data = instructions.split(",");
 		String[]timeTemp = data[0].split("[:\\.]");
 		inputTime=Integer.parseInt(timeTemp[0])*3600000+Integer.parseInt(timeTemp[1])*60000+Integer.parseInt(timeTemp[2])*1000+Integer.parseInt(timeTemp[3]);
+		if (currentTime > inputTime){
+			System.out.println("Floor subsystem: Input occurred before the previous input");
+			System.exit(1);
+		}
 		if(currentTime==0) {
 			currentTime=inputTime;
 		}
 		floorNumber = Integer.parseInt(data[1]);
 		destination = Integer.parseInt(data[2]);
 
-		control.dataIn = true;
 	}
+	
+
 	/**
 	 * runs the floor thread
 	 */
 	public void run() {
 		try {
+			 
 			//If there is still a line of buffered reader with instructions execute read data
 			while((instructions = input.readLine()) !=null) {
-				readData();
-			//When all of the instructions are read there is a pause and then the floor number
-			//and destination are added to control system.
-			try {
-					Thread.sleep(inputTime-currentTime);
-					System.out.println("input recieved after "+ (inputTime-currentTime)+" ms, sending data to scheduler in ");
-					currentTime=inputTime;
-					control.inputFloor.add(floorNumber);
-					control.destination.add(destination);
+				if (instructions.matches("\\d{2}:\\d{2}:\\d{2}:\\d{2},\\d{2},\\d{2}")) {
+					readData();
+					//When all of the instructions are read there is a pause and then the floor number
+					//and destination are added to control system.
+					try {
+						Thread.sleep(inputTime-currentTime);
+						System.out.println("Floor Subsystem: input recieved after "+ (inputTime-currentTime)+" ms, sending data to scheduler");
+						currentTime=inputTime;
+						scheduler.inputFloors.add(floorNumber);
+						scheduler.destinations.add(destination);
+						scheduler.receiveData(floorNumber);
+					}
+					catch (InterruptedException e) {
+					
+					}
+				}
+				else {
+					System.out.println("Floor subsystem: invalid inputfile format");
+					break;
+				}
 			}
-			catch (InterruptedException e) {
-				
-			}
-			}
-		} catch (IOException e) {
-			
 		}
+		catch (IOException e) {
+				
+		}	
 	}
-	
+	public void sendData (byte[] data, InetAddress IP, int port) {
+		
+		sendPacket = new DatagramPacket(data, data.length, IP, schedulerPort);
+		System.out.println("Elevator: Sending packet:");
+		System.out.println("To host: " + sendPacket.getAddress());
+		System.out.println("Destination host port: " + sendPacket.getPort());
+		int len = sendPacket.getLength();
+		System.out.println("Length: " + len);
+		System.out.print("Containing: ");
+		System.out.println(Arrays.toString(data)); // or could print "s"
+		try {
+			sendReceiveSocket.send(sendPacket);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		System.out.println("Elevator: Packet sent.\n");
+	}
 }
